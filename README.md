458ce8df21238caab946bca69fea504f0e13732c
Amanda <amandaannegeorge@gmail.com>
2015-08-29 17:29:33 -0700

    got search results array on viewpage

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# retirement-hack
+# Retirement Hack
+
+## Description
+Retirement Hack is an app where people can learn how to retire early and/or find ways to minimize expenses and maximize income during retirement.
+
+## Technologies Used
+- JavaScript
+- AngularJS
+- Bootstrap CSS
+- Google Custom Search API
--- app.js
+++ app.js
@@ -1,6 +1,20 @@
 angular.module("retirementHack", [])
-	.controller("MainCtrl", ["$scope", function ($scope) {
-		$scope.results = {};
+	.controller("MainCtrl", ["$scope", "$http", function ($scope, $http) {
+		$scope.results = [];
+
+		$scope.searchWords = function () {
+			var keywords = $scope.keywords.replace(/\s+/, '%20');
+			var url = "https://www.googleapis.com/customsearch/v1?cx=002024384976429766137:lw0vcl3ockg&q=" + keywords + "&num=10&key=AIzaSyDYIMs_cxxV7aq4JFbr6_cOvzS3eV_F1rk";
+			$http.get(url)
+				.then(function (response) {
+					console.log(response.data.items);
+					$scope.keywords = '';
+					$scope.results = response.data.items;
+					// results[i].title
+					// results[i].link
+
+				});
+		};
 	}])
 
-// "https://www.googleapis.com/customsearch/v1?cx=002024384976429766137:lw0vcl3ockg&q=" + $scope.term + "&num=10&key=AIzaSyDYIMs_cxxV7aq4JFbr6_cOvzS3eV_F1rk"
+	// https://www.googleapis.com/customsearch/v1?cx=002024384976429766137:lw0vcl3ockg&q=retire%20early&num=10&key=AIzaSyDYIMs_cxxV7aq4JFbr6_cOvzS3eV_F1rk
